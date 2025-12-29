5e3042ddc2ff83f983ff86450683dd3f2bea894c
Isaac Dream <isaackprogramming44524d@gmail.com>
2025-12-29 20:14:31 +0200

    finished the code the playing effect is not working

--- 01-Javascript-Drum-Kit-Solution/script.js
+++ 01-Javascript-Drum-Kit-Solution/script.js
@@ -1,5 +1,18 @@
-window.addEventListener("keydown", function(e) {
-    const audio = this.document.querySelector(`audio[data-key = "${e.keyCode}"]`)
+function removeTransition(e) {
+    if (e.propertyName !== 'transform') return;
+    this.classList.remove('playing');
+}
+
+function playSound(e) {
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
     if (!audio) return;
-    audio.play()
-})
+
+    key.classList.add('playing');
+    audio.currentTime = 0;
+    audio.play();
+}
+
+const keys = document.querySelectorAll('.key');
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+window.addEventListener('keydown', playSound);
